7ca41215b2943f2ad109996f2cf81f58db802536
Zach <zach@pal-nat186-61-77.itap.purdue.edu>
2019-01-09 18:43:58 -0500

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Zach on 1/9/19.
+//  Copyright © 2019 Zach Royko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
