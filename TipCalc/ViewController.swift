6e3584c6cf474a782c99e7cdfbc1b638938f744f
Zach <zach@Zachs-MBP.local>
2019-01-12 22:09:20 -0500

    first commit

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func keyboadTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format:"$%.2f",total)
+    }
+    
 }
 
